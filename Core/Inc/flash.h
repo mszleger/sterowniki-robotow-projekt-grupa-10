fc6d8ff301646ee5db5ebb5384bad714c19ecedf
Michal Szleger <michal.szleger@gmail.com>
2023-04-25 11:56:52 +0200

    Add read/write from/to external flash functions

--- Core/Inc/flash.h
+++ Core/Inc/flash.h
@@ -5,6 +5,11 @@
 #include <stdio.h>
 #include "quadspi.h"
 
+/* Configuration */
+#define FLASH_EXT_SIZE               	   		0x2000000 /* 256 MBits*/
+#define FLASH_EXT_SECTOR_SIZE              	   	0x10000   /* 64kBytes */
+#define FLASH_EXT_PAGE_SIZE                		0x100     /* 256 bytes */
+
 /* Command Definitions */
 // Reset operations
 #define CMD_RESET_ENABLE 0x66
@@ -86,3 +91,50 @@ void flash_print_enhanced_volatile_configuration_register();
  * \brief Wysyła za pomocą USART odczytaną z zewnętrznej pamięci flash konfigurację rejestru flag
  */
 void flash_print_flag_status_register();
+
+/**
+ * \brief Wstrzymuje pracę programu do czasu zakończenia zapisu do zewnętrznej pamięci flash
+ *
+ * \return Zwraca HAL_OK, jeśli operacja się powiodła lub HAL_ERROR jeśli nie.
+ */
+uint8_t flash_wait_for_ready();
+
+/**
+ * \brief Załącza tryb zapisu zewnętrznej pamięci flash
+ *
+ * \return Zwraca HAL_OK, jeśli operacja się powiodła lub HAL_ERROR jeśli nie.
+ */
+uint8_t flash_write_enable();
+
+/**
+ * \brief Zapisuje dane z bufora o danej długości w zewnętrznej pamięci flash zaczynając od danego adresu
+ *
+ * @param start_address - adres pierwszej komórki obszaru, do którego mają zostać zapisane dane
+ * @param buffer - wskaźnik na bufor z danymi do zapisu
+ * @param buffer_size - wielkość bufora
+ *
+ * \return Zwraca HAL_OK, jeśli operacja się powiodła lub HAL_ERROR jeśli nie.
+ */
+uint8_t flash_write(uint32_t start_address, uint8_t *buffer, uint32_t buffer_size);
+
+/**
+ * \brief Odczytuje zestaw danch o zadanej długości z zewnętrznej pamięci flash zaczynając od danego adresu do bufora
+ *
+ * @param start_address - adres pierwszej komórki obszaru, z którego mają zostać odczytane dane
+ * @param buffer - wskaźnik na bufor na odczytane dane
+ * @param buffer_size - wielkość obaszaru danych do odczytu
+ *
+ * \return Zwraca HAL_OK, jeśli operacja się powiodła lub HAL_ERROR jeśli nie.
+ */
+uint8_t flash_read(uint32_t start_address, uint8_t *buffer, uint32_t buffer_size);
+
+/**
+ * \brief Funkcja do testowania operacji odczytu/zapisu do/z zewnętrznej pamięci flash
+ *
+ * Zapisuje bufor danych do zewnętrznej pamięci flash, później odczytuje zapisane dane, porównuje z danymi początkowymi i wysyła za pomocą USART wynik testu.
+ */
+void flash_test_read_write();
+
+
+
+
--- Core/Src/flash.c
+++ Core/Src/flash.c
@@ -214,3 +214,126 @@ void flash_print_flag_status_register() {
 	else
 		printf("      -> Protection: Clear\r\n");
 }
+
+uint8_t flash_wait_for_ready() {
+    QSPI_CommandTypeDef flash_cmd;
+    flash_cmd.InstructionMode 		= QSPI_INSTRUCTION_1_LINE;
+    flash_cmd.Instruction 			= CMD_READ_STATUS_REGISTER;
+    flash_cmd.AddressMode 			= QSPI_ADDRESS_NONE;
+    flash_cmd.AlternateByteMode 		= QSPI_ALTERNATE_BYTES_NONE;
+    flash_cmd.DataMode 				= QSPI_DATA_1_LINE;
+    flash_cmd.DummyCycles 			= 0;
+    flash_cmd.DdrMode 				= QSPI_DDR_MODE_DISABLE;
+    flash_cmd.DdrHoldHalfCycle 		= QSPI_DDR_HHC_ANALOG_DELAY;
+    flash_cmd.SIOOMode 				= QSPI_SIOO_INST_EVERY_CMD;
+
+    QSPI_AutoPollingTypeDef flash_cnf;
+    flash_cnf.Match 					= 0x00;
+    flash_cnf.Mask 					= 0x01;
+    flash_cnf.MatchMode 				= QSPI_MATCH_MODE_AND;
+    flash_cnf.StatusBytesSize 		= 1;
+    flash_cnf.Interval 				= 0x10;
+    flash_cnf.AutomaticStop 			= QSPI_AUTOMATIC_STOP_ENABLE;
+    if(HAL_QSPI_AutoPolling(&hqspi, &flash_cmd, &flash_cnf, HAL_QPSI_TIMEOUT_DEFAULT_VALUE) != HAL_OK) return HAL_ERROR;
+
+    return HAL_OK;
+}
+
+uint8_t flash_write_enable() {
+	QSPI_CommandTypeDef flash_cmd;
+    flash_cmd.InstructionMode 		= QSPI_INSTRUCTION_1_LINE;
+    flash_cmd.Instruction 			= CMD_WRITE_ENABLE;
+    flash_cmd.AddressMode 			= QSPI_ADDRESS_NONE;
+    flash_cmd.AlternateByteMode 	= QSPI_ALTERNATE_BYTES_NONE;
+    flash_cmd.DataMode 				= QSPI_DATA_NONE;
+    flash_cmd.DummyCycles 			= 0;
+    flash_cmd.DdrMode 				= QSPI_DDR_MODE_DISABLE;
+    flash_cmd.DdrHoldHalfCycle 		= QSPI_DDR_HHC_ANALOG_DELAY;
+    flash_cmd.SIOOMode 				= QSPI_SIOO_INST_EVERY_CMD;
+    if(HAL_QSPI_Command(&hqspi, &flash_cmd, HAL_QPSI_TIMEOUT_DEFAULT_VALUE) != HAL_OK) return HAL_ERROR;
+
+    QSPI_AutoPollingTypeDef flash_cnf;
+	flash_cnf.Match 				= 0x02;
+    flash_cnf.Mask 					= 0x02;
+    flash_cnf.MatchMode 			= QSPI_MATCH_MODE_AND;
+    flash_cnf.StatusBytesSize 		= 1;
+    flash_cnf.Interval 				= 0x10;
+    flash_cnf.AutomaticStop 		= QSPI_AUTOMATIC_STOP_ENABLE;
+    flash_cmd.Instruction 			= CMD_READ_STATUS_REGISTER;
+    flash_cmd.DataMode 				= QSPI_DATA_1_LINE;
+    if(HAL_QSPI_AutoPolling(&hqspi, &flash_cmd, &flash_cnf, HAL_QPSI_TIMEOUT_DEFAULT_VALUE) != HAL_OK) return HAL_ERROR;
+
+	return HAL_OK;
+}
+
+uint8_t flash_write(uint32_t start_address, uint8_t *buffer, uint32_t buffer_size) {
+	QSPI_CommandTypeDef flash_cmd;
+	uint32_t stop_address = start_address + buffer_size;
+	uint32_t current_address = start_address;
+    uint32_t current_size = FLASH_EXT_PAGE_SIZE - (start_address % FLASH_EXT_PAGE_SIZE); // Number of bytes to end of page
+    if(current_size > buffer_size) current_size = buffer_size;
+
+    flash_cmd.InstructionMode 		= QSPI_INSTRUCTION_1_LINE;
+    flash_cmd.AddressSize 			= QSPI_ADDRESS_32_BITS;
+    flash_cmd.AlternateByteMode 	= QSPI_ALTERNATE_BYTES_NONE;
+    flash_cmd.DdrMode 				= QSPI_DDR_MODE_DISABLE;
+    flash_cmd.DdrHoldHalfCycle 		= QSPI_DDR_HHC_ANALOG_DELAY;
+    flash_cmd.SIOOMode 				= QSPI_SIOO_INST_EVERY_CMD;
+    flash_cmd.Instruction 			= CMD_QUAD_INPUT_FAST_PROGRAM;
+    flash_cmd.AddressMode 			= QSPI_ADDRESS_1_LINE;
+    flash_cmd.DataMode 				= QSPI_DATA_4_LINES;
+    flash_cmd.NbData 				= buffer_size;
+    flash_cmd.Address 				= start_address;
+    flash_cmd.DummyCycles 			= 0;
+
+    do { // Write page by page
+    	if (current_size == 0) return HAL_OK;
+        flash_write_enable();
+        flash_cmd.Address 			= current_address;
+        flash_cmd.NbData 			= current_size;
+        if (HAL_QSPI_Command(&hqspi, &flash_cmd, HAL_QPSI_TIMEOUT_DEFAULT_VALUE) != HAL_OK) return HAL_ERROR;
+        if (HAL_QSPI_Transmit(&hqspi, buffer, HAL_QPSI_TIMEOUT_DEFAULT_VALUE) != HAL_OK) return HAL_ERROR;
+        if (flash_wait_for_ready() != HAL_OK) return HAL_ERROR;
+
+        current_address += current_size;
+        buffer += current_size; // Shifting front of buffer
+        if((current_address + FLASH_EXT_PAGE_SIZE) > stop_address) current_size = stop_address - current_address;
+        else current_size = FLASH_EXT_PAGE_SIZE;
+    } while(current_address <= stop_address);
+
+    return HAL_OK;
+}
+
+uint8_t flash_read(uint32_t start_address, uint8_t *buffer, uint32_t buffer_size) {
+	QSPI_CommandTypeDef flash_cmd;
+    flash_cmd.InstructionMode 		= QSPI_INSTRUCTION_1_LINE;
+    flash_cmd.AddressSize 			= QSPI_ADDRESS_32_BITS;
+    flash_cmd.AlternateByteMode 	= QSPI_ALTERNATE_BYTES_NONE;
+    flash_cmd.DdrMode 				= QSPI_DDR_MODE_DISABLE;
+    flash_cmd.DdrHoldHalfCycle 		= QSPI_DDR_HHC_ANALOG_DELAY;
+    flash_cmd.SIOOMode 				= QSPI_SIOO_INST_EVERY_CMD;
+    flash_cmd.Instruction 			= CMD_QUAD_INPUT_OUTPUT_FAST_READ;
+    flash_cmd.AddressMode 			= QSPI_ADDRESS_1_LINE;
+    flash_cmd.DataMode 				= QSPI_DATA_4_LINES;
+    flash_cmd.NbData 				= buffer_size;
+    flash_cmd.Address 				= start_address;
+    flash_cmd.DummyCycles 			= 10;
+	if(HAL_QSPI_Command(&hqspi, &flash_cmd, HAL_QSPI_TIMEOUT_DEFAULT_VALUE) != HAL_OK) return HAL_ERROR;
+	if(HAL_QSPI_Receive(&hqspi, buffer, HAL_QSPI_TIMEOUT_DEFAULT_VALUE) != HAL_OK) return HAL_ERROR;
+	return HAL_OK;
+}
+
+void flash_test_read_write() {
+	uint8_t data_write[1024];
+	uint8_t data_read[1024];
+	for(uint32_t i = 0; i < 1024; ++i) data_write[i] = i % 256;
+	if(flash_write(0, data_write, 1024) != HAL_OK) printf("Error while writing to flash\n\r");
+	if(flash_read(0, data_read, 1024) != HAL_OK) printf("Error while reading from flash\n\r");
+	for(uint32_t i = 0; i < 1024; ++i)
+		if(data_write[i] != data_read[i])
+			printf("Error - incorrect data on index %lu: write -> %d, read -> %d\n\r", i, data_write[i], data_read[i]);
+}
+
+
+
+
