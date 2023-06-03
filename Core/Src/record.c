ed3ab06e0d8026b1b5782b3c3626800b0b507c1b
MarekWlazloo <130281067+MarekWlazloo@users.noreply.github.com>
2023-06-03 16:06:51 +0200

    Update record.c

--- Core/Src/record.c
+++ Core/Src/record.c
@@ -20,9 +20,11 @@ void Select_sample_rate(int w){
 }
 
 void micro_init(int sample_choice){   // 	TO WYWOLUJE RAZ PRZED WHILE(1) W MAINIE
+
 	Select_sample_rate(sample_choice);
-	if(sample_rate == -1){
-		sample_rate = BSP_AUDIO_FREQUENCY_48K; // bez znaczenia bo w init jest wpisane na stale
+
+	if(sample_rate == -1){// bez znaczenia bo w init jest wpisane na stale
+		sample_rate = BSP_AUDIO_FREQUENCY_48K;
 	}
 
 	  /* Initialize audio input */
@@ -89,11 +91,11 @@ void micro_deinit(){ // TEGO NIGDZIE NIE WYWOLUJE
 	  }
 
 }*/
-void record()
+void record() // to wywoluj w petli while
 {
  AudioRecordExit = 0;
 RecordBufferOffset = BUFFER_OFFSET_NONE;
-
+i = 0;
 HAL_Delay(3000);
 // callback function pointers
 BSP_AUDIO_IN_RegisterCallbacks(AudioRecord_Error_CallBack,AudioRecord_HalfTransfer_CallBack,
@@ -102,7 +104,7 @@ BSP_AUDIO_IN_RegisterCallbacks(AudioRecord_Error_CallBack,AudioRecord_HalfTransf
   uint16_t recordBuffer[BUFF_SIZE];
 
   /* Start the audio record */
-  if (BSP_AUDIO_IN_Record((uint16_t*)&recordBuffer[0],
+  if (BSP_AUDIO_IN_Record((uint16_t*)&recordBuffer[0], // to normalnie dziala, nie zwraca bledu
 		  BUFF_SIZE) != AUDIO_OK)
   {
 	    printf("NIE NAGRYWA SIE  \r\n" );
@@ -115,20 +117,21 @@ BSP_AUDIO_IN_RegisterCallbacks(AudioRecord_Error_CallBack,AudioRecord_HalfTransf
   /* Wait for the audio record to complete */
  // HAL_Delay(5000);
 
-//  while (AudioRecordExit != SET)
-//  {
-//	 // HAL_Delay(1000);
-//	  //save recorded samples to FLASH
-//	 // printf("NAGRYWANE PROBKI: %d    i:  %d\r\n", RecordBuff[i], i);
-////	  i++;
-//	//  HAL_Delay(1);
-////	  if (i == (BUFF_SIZE - 1)){
-////		  AudioRecordExit = SET;
-////	  }
-//	  if (RecordBufferOffset == BUFFER_OFFSET_FULL){
+  while (AudioRecordExit != SET)
+  {
+	 // HAL_Delay(1000);
+	  //save recorded samples to FLASH
+	 // printf("NAGRYWANE PROBKI: %d    i:  %d\r\n", RecordBuff[i], i);
+	  i++;
+	  HAL_Delay(1);
+	  if (i == (BUFF_SIZE - 1)){
+		  AudioRecordExit = SET;
+	  }
+//	  if (RecordBufferOffset == BUFFER_OFFSET_FULL){ // warunek spelniony gdy wykona sie AudioRecord_TransferComplete_CallBack() ale probki sie nie nagrywaja i nie wychodzi z warunku
 //		  AudioRecordExit = SET;
+//
 //	  }
-//  }
+  }
 
   /* Stop the audio record */
   if (BSP_AUDIO_IN_Stop() != AUDIO_OK)
@@ -139,6 +142,7 @@ BSP_AUDIO_IN_RegisterCallbacks(AudioRecord_Error_CallBack,AudioRecord_HalfTransf
   else{
 	  printf("nagrywanie zatrzymane\r\n");
   }
+  HAL_Delay(2000);
 
   /* Display recorded samples */
   for (uint32_t i = 0; i < BUFF_SIZE; i++)
@@ -180,7 +184,7 @@ void AudioRecord_Error_CallBack(void)
 	printf("BLAD AUDIO ERROR CALLBACK \r\n");
 }
 
-void NoiseLVL() {
+void NoiseLVL() { // na to narazie nie patrz bo jak record() nie działa to to tym bardziej nie bedzie dzialac
 	AudioRecordNoiseLVLExit = RESET;
 	start_recording = RESET;
 	int i = 0;
@@ -211,4 +215,3 @@ void NoiseLVL() {
 		  record();
 	  }
 }
-
