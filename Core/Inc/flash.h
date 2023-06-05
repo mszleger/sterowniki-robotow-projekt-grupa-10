570d9017c3bba827fbc547e8590f2b3965363c1e
Michal Szleger <michal.szleger@gmail.com>
2023-06-05 03:32:39 +0200

    Add flash module with BSP init function

--- Core/Inc/flash.h
+++ Core/Inc/flash.h
@@ -1,9 +1,10 @@
 #pragma once
 
 // ----- SETTINGS ----- //
-#define TRACK_AMOUNT            5
-#define TRACK_SPACE_LENGTH      ((uint32_t)0x0200)
-#define USER_DATA_BEGIN_ADDRESS ((uint32_t)0x0050)
+#define TRACK_AMOUNT            8
+#define SUBSECTORS_PER_TRACK    511
+
+//#define FLASH_DEBUG_VERBOSE
 
 // Flash chip: N25Q128A13EF840E
 
@@ -12,8 +13,14 @@
 #include "stm32l476g_discovery.h"
 #include "stm32l476g_discovery_qspi.h"
 
+#define TRACK_STATUS_SUBSECTOR 1
+#define SUBSECTOR_SIZE      4096
+
 QSPI_Info flash_info;
-uint32_t track_status_address[TRACK_AMOUNT];
+
+uint32_t track_status_page_start_address;
+uint8_t  track_status_value[TRACK_AMOUNT];
+
 uint32_t track_begin_address[TRACK_AMOUNT];
 
 uint32_t record_address;
@@ -22,62 +29,17 @@ uint32_t record_end_address; // start address of next track
 uint32_t play_address;
 uint32_t play_end_address; // start address of next track
 
-/**
- *
- */
+
+/* ---------- USER INTERFACE ---------- */
 HAL_StatusTypeDef Flash_Init(void);
 
-/**
- *
- */
-HAL_StatusTypeDef Flash_Init_Tracks_Layout(void);
-
-/**
- *
- */
-HAL_StatusTypeDef Flash_Test(void);
-
-
-
-/**
- *
- */
-HAL_StatusTypeDef Flash_Track_Status_Set(uint8_t track_number, uint8_t new_track_status);
-
-/**
- *
- */
+HAL_StatusTypeDef Flash_Track_Status_Set(uint8_t track_number, uint8_t  track_status);
 HAL_StatusTypeDef Flash_Track_Status_Get(uint8_t track_number, uint8_t* track_status);
 
-/**
- *
- */
-HAL_StatusTypeDef Flash_Record_Start(uint8_t track_number);
+HAL_StatusTypeDef Flash_Track_Record_Start(uint8_t track_number);
+uint32_t Flash_Track_Record_Buffer_Save(uint8_t* buffer, uint32_t buffer_length);
 
-/**
- *
- */
-uint32_t Flash_Record_Save_Buffer(uint8_t* buffer, uint32_t buffer_length);
+HAL_StatusTypeDef Flash_Track_Play_Start(uint8_t track_number);
+uint32_t Flash_Track_Play_Buffer_Read(uint8_t* buffer, uint32_t buffer_length);
 
-/**
- *
- */
-HAL_StatusTypeDef Flash_Play_Start(uint8_t track_number);
-
-/**
- *
- */
-uint32_t Flash_Play_Read_Buffer(uint8_t* buffer, uint32_t buffer_length);
-
-
-
-
-
-
-
-
-
-
-
-
-
+HAL_StatusTypeDef Flash_Test(void);
--- Core/Src/flash.c
+++ Core/Src/flash.c
@@ -1,9 +1,257 @@
 #include "flash.h"
 
+static HAL_StatusTypeDef Flash_Track_Load_Statuses() {
+	#ifdef FLASH_DEBUG_VERBOSE
+		printf("FLASH: Flash_Track_Load_Statuses\r\n");
+	#endif
+	return BSP_QSPI_Read(track_status_value, track_status_page_start_address, TRACK_AMOUNT);
+}
+
+static HAL_StatusTypeDef Flash_Init_Tracks_Layout(void) {
+	#ifdef FLASH_DEBUG_VERBOSE
+		printf("FLASH: Flash_Init_Tracks_Layout\r\n");
+	#endif
+	HAL_StatusTypeDef status;
+	if((TRACK_STATUS_SUBSECTOR + (TRACK_AMOUNT * SUBSECTORS_PER_TRACK)) * SUBSECTOR_SIZE > flash_info.FlashSize) {
+		printf("     - Not enough space for given track amount and size\r\n");
+		return HAL_ERROR;
+	}
+	track_status_page_start_address = 0;
+	track_begin_address[0] = (TRACK_STATUS_SUBSECTOR) * SUBSECTOR_SIZE;
+	for(uint8_t i = 1; i < TRACK_AMOUNT; ++i) {
+		track_begin_address[i] = track_begin_address[i - 1] + (SUBSECTORS_PER_TRACK * SUBSECTOR_SIZE);
+		#ifdef FLASH_DEBUG_VERBOSE
+			printf("     - Track %d begin address: %ld\r\n", i, track_begin_address[i]);
+		#endif
+	}
+	status = Flash_Track_Load_Statuses();
+	if(status != HAL_OK) {
+		printf("     - Failed to load tracks' statuses\r\n");
+		return status;
+	}
+	return HAL_OK;
+}
+
+static HAL_StatusTypeDef Flash_Test_All_Tracks_Statuses() {
+	#ifdef FLASH_DEBUG_VERBOSE
+		printf("FLASH: Flash_Test_All_Tracks_Statuses\r\n");
+	#endif
+	HAL_StatusTypeDef status;
+	uint8_t buffer;
+	uint8_t test_values[2] = {0x00, 0xff};
+	uint8_t test_values_amount = 2;
+	for(uint8_t test_value_id = 0; test_value_id < test_values_amount; ++test_value_id) {
+		for(uint8_t track_status_id = 0; track_status_id < TRACK_AMOUNT; ++track_status_id) {
+			status = Flash_Track_Status_Set(track_status_id, test_values[test_value_id]);
+			if(status != HAL_OK) {
+				printf("     - Failed to set status of track %d\r\n", track_status_id);
+				return status;
+			}
+			status = Flash_Track_Status_Get(track_status_id, &buffer);
+			if(status != HAL_OK) {
+				printf("     - Failed to read status of track %d\r\n", track_status_id);
+				return status;
+			}
+			if(buffer != test_values[test_value_id]) {
+				printf("     - Test failed: Value of track status %d is equal to %d instead of %d\r\n", track_status_id, buffer, test_values[test_value_id]);
+				return HAL_ERROR;
+			}
+			#ifdef FLASH_DEBUG_VERBOSE
+				printf("     - Correct value of track %d status: readed %d; expected %d\r\n", track_status_id, buffer, test_values[test_value_id]);
+			#endif
+		}
+	}
+	return HAL_OK;
+}
+
+static HAL_StatusTypeDef Flash_Test_Record_And_Play() {
+	#ifdef FLASH_DEBUG_VERBOSE
+		printf("FLASH: Flash_Test_Record_And_Play\r\n");
+	#endif
+	HAL_StatusTypeDef status;
+	uint8_t buffer_tx[256];
+	uint8_t buffer_rx[256];
+	uint32_t saved_buffers_amount = 0;
+	uint32_t correct_readed_buffers_amount = 0;
+	for(uint32_t i = 0; i < 256; ++i)
+		buffer_tx[i] = i;
+	for(uint8_t track_id = 0; track_id < TRACK_AMOUNT; ++track_id) {
+		status = Flash_Track_Record_Start(track_id);
+		if(status != HAL_OK){
+			printf("     - Failed to start recording of track %d\r\n", track_id);
+			return status;
+		}
+		while(Flash_Track_Record_Buffer_Save(buffer_tx, 256) == 256)
+			++saved_buffers_amount;
+		status = Flash_Track_Play_Start(track_id);
+		if(status != HAL_OK) return status;
+		while(Flash_Track_Play_Buffer_Read(buffer_rx, 256) == 256) {
+			for(uint32_t i = 0; i < 256; ++i) {
+				if(buffer_rx[i] != buffer_tx[i]) {
+					printf("     - buffer tx and rx not equal at index %ld: rx = %d, tx = %d\r\n", i, buffer_rx[i], buffer_tx[i]);
+					--correct_readed_buffers_amount;
+					break;
+				}
+			}
+			++correct_readed_buffers_amount;
+		}
+	}
+	#ifdef FLASH_DEBUG_VERBOSE
+		printf("     - saved %ld buffers, readed %ld correct buffers\r\n", saved_buffers_amount, correct_readed_buffers_amount);
+	#endif
+	if(saved_buffers_amount != correct_readed_buffers_amount) {
+		printf("     - testing recording and playing failed: saved buffers amount (%ld) != correctly readed buffers amount (%ld)\r\n", saved_buffers_amount, correct_readed_buffers_amount);
+		return HAL_ERROR;
+	}
+	return HAL_OK;
+}
+
+HAL_StatusTypeDef Flash_Track_Status_Set(uint8_t track_number, uint8_t new_track_status) {
+	#ifdef FLASH_DEBUG_VERBOSE
+		printf("FLASH: Flash_Track_Status_Set\r\n");
+	#endif
+	HAL_StatusTypeDef status;
+	uint8_t track_status_value_copy[TRACK_AMOUNT];
+	if(track_number >= TRACK_AMOUNT) {
+		printf("     - Incorrect track number: %d\r\n", track_number);
+		return HAL_ERROR;
+	}
+	for(uint8_t i = 0; i < TRACK_AMOUNT; ++i)
+		track_status_value_copy[i] = track_status_value[i];
+	track_status_value_copy[track_number] = new_track_status;
+	status = BSP_QSPI_Erase_Block(track_status_page_start_address);
+	if(status != HAL_OK) {
+		printf("     - Failed to erase track status block\r\n");
+		return status;
+	}
+	status = BSP_QSPI_Write(track_status_value_copy, track_status_page_start_address, TRACK_AMOUNT);
+	if(status != HAL_OK) {
+		printf("     - Failed to save tracks' statuses\r\n");
+		return status;
+	}
+	status = Flash_Track_Load_Statuses();
+	if(status != HAL_OK) {
+		printf("     - Failed to load tracks' statuses\r\n");
+		return status;
+	}
+	return HAL_OK;
+}
+
+HAL_StatusTypeDef Flash_Track_Status_Get(uint8_t track_number, uint8_t* track_status) {
+	#ifdef FLASH_DEBUG_VERBOSE
+		printf("FLASH: Flash_Track_Status_Get\r\n");
+	#endif
+	if(track_number >= TRACK_AMOUNT) {
+		printf("     - Incorrect track number: %d\r\n", track_number);
+		return HAL_ERROR;
+	}
+	*track_status = track_status_value[track_number];
+	return HAL_OK;
+}
+
+HAL_StatusTypeDef Flash_Track_Record_Start(uint8_t track_number) {
+	#ifdef FLASH_DEBUG_VERBOSE
+		printf("FLASH: Flash_Record_Start\r\n");
+	#endif
+	HAL_StatusTypeDef status;
+	if(track_number >= TRACK_AMOUNT) {
+		printf("     - Incorrect track number: %d\r\n", track_number);
+		return HAL_ERROR;
+	}
+	record_address = track_begin_address[track_number];
+	record_end_address = track_begin_address[track_number] + (SUBSECTORS_PER_TRACK * SUBSECTOR_SIZE);
+	for(uint32_t i = 0; i < SUBSECTORS_PER_TRACK; ++i) {
+		status = BSP_QSPI_Erase_Block(track_begin_address[track_number] + (i * SUBSECTOR_SIZE));
+		if(status != HAL_OK) {
+			printf("     - Failed to erase sector: %ld\r\n", track_begin_address[track_number] + (i * SUBSECTOR_SIZE));
+			return status;
+		}
+	}
+	return HAL_OK;
+}
+
+uint32_t Flash_Track_Record_Buffer_Save(uint8_t* buffer, uint32_t buffer_length) {
+	#ifdef FLASH_DEBUG_VERBOSE
+		printf("FLASH: Flash_Record_Save_Buffer\r\n");
+	#endif
+	if(buffer_length > record_end_address - record_address)
+		buffer_length = record_end_address - record_address;
+	if(buffer_length == 0) return 0;
+	if(BSP_QSPI_Write(buffer, record_address, buffer_length) != HAL_OK) {
+		printf("     - Failed to write track to flash\r\n");
+		return 0;
+	}
+	record_address += buffer_length;
+	return buffer_length;
+}
+
+HAL_StatusTypeDef Flash_Track_Play_Start(uint8_t track_number) {
+	#ifdef FLASH_DEBUG_VERBOSE
+		printf("FLASH: Flash_Play_Start\r\n");
+	#endif
+	if(track_number >= TRACK_AMOUNT) {
+		printf("     - Incorrect track number: %d\r\n", track_number);
+		return HAL_ERROR;
+	}
+	play_address = track_begin_address[track_number];
+	play_end_address = track_begin_address[track_number] + (SUBSECTORS_PER_TRACK * SUBSECTOR_SIZE);
+	return HAL_OK;
+}
+
+uint32_t Flash_Track_Play_Buffer_Read(uint8_t* buffer, uint32_t buffer_length) {
+	#ifdef FLASH_DEBUG_VERBOSE
+		printf("FLASH: Flash_Play_Read_Buffer\r\n");
+	#endif
+	if(buffer_length > play_end_address - play_address)
+		buffer_length = play_end_address - play_address;
+	if(buffer_length == 0) return 0;
+	if(BSP_QSPI_Read(buffer, play_address, buffer_length) != HAL_OK) {
+		printf("     - Failed to read track from flash\r\n");
+		return 0;
+	}
+	play_address += buffer_length;
+	return buffer_length;
+}
+
+HAL_StatusTypeDef Flash_Test(void) {
+	#ifdef FLASH_DEBUG_VERBOSE
+		printf("FLASH: Flash_Test\r\n");
+	#endif
+	HAL_StatusTypeDef status;
+	status = Flash_Test_All_Tracks_Statuses();
+	if(status != HAL_OK) {
+		printf("     - Testing all tracks' statuses failed\r\n");
+		return status;
+	}
+	status = Flash_Test_Record_And_Play();
+	if(status != HAL_OK) {
+		printf("     - Testing recording and playing failed\r\n");
+		return status;
+	}
+	return HAL_OK;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 HAL_StatusTypeDef Flash_Init(void) {
+	#ifdef FLASH_DEBUG_VERBOSE
+		printf("FLASH: Flash_Init\r\n");
+	#endif
 	HAL_StatusTypeDef status;
 
-	status = BSP_QSPI_Init();
+	status = BSP_QSPI_Init(); // TODO: wyrzucić init BSP
 	if(status != HAL_OK) return status;
 
     flash_info.FlashSize          = (uint32_t)0x00;
@@ -26,182 +274,3 @@ HAL_StatusTypeDef Flash_Init(void) {
 
     return HAL_OK;
 }
-
-HAL_StatusTypeDef Flash_Init_Tracks_Layout(void) {
-	if(USER_DATA_BEGIN_ADDRESS + TRACK_AMOUNT + (TRACK_AMOUNT * TRACK_SPACE_LENGTH) > flash_info.FlashSize)
-		return HAL_ERROR;
-
-	for(uint32_t i = 0; i < TRACK_AMOUNT; ++i)
-		track_status_address[i] = USER_DATA_BEGIN_ADDRESS + i;
-
-	for(uint32_t i = 0; i < TRACK_AMOUNT; ++i)
-		track_begin_address[i] = USER_DATA_BEGIN_ADDRESS + TRACK_AMOUNT + (i * TRACK_SPACE_LENGTH);
-
-	return HAL_OK;
-}
-
-HAL_StatusTypeDef Flash_Test(void) {
-	uint32_t buffer_tx_size = 256;
-	uint32_t buffer_rx_size = 100;
-
-	HAL_StatusTypeDef status;
-	uint8_t buffer_tx[buffer_tx_size];
-	uint8_t buffer_rx[buffer_rx_size];
-	uint32_t bytes_tested = 0;
-
-	// Filling tx buffer
-	for(uint32_t i = 0; i < buffer_tx_size; ++i)
-		buffer_tx[i] = i % 256;
-
-	// Testing all records RW space
-	for(uint8_t track = 0; track < TRACK_AMOUNT; ++track) {
-		// Testing track status register
-		uint8_t status_register_value = 0;
-		status = Flash_Track_Status_Set(track, 150);
-		if(status != HAL_OK) return status;
-		status = Flash_Track_Status_Get(track, &status_register_value);
-		if(status != HAL_OK) return status;
-		if(status_register_value != 150) return HAL_ERROR;
-
-		status = Flash_Track_Status_Set(track, 10);
-		if(status != HAL_OK) return status;
-		status = Flash_Track_Status_Get(track, &status_register_value);
-		if(status != HAL_OK) return status;
-		if(status_register_value != 10) return HAL_ERROR;
-
-		// Start of recording
-		status = Flash_Record_Start(track);
-		if(status != HAL_OK) return status;
-		// Recording
-		while(Flash_Record_Save_Buffer(buffer_tx, buffer_tx_size) != buffer_tx_size);
-		// Start of playing
-		status = Flash_Play_Start(track);
-		if(status != HAL_OK) return status;
-		// Playing and values checking
-		uint8_t expected_value = 0;
-		uint32_t bytes_read = 0;
-		do {
-			bytes_read = Flash_Record_Save_Buffer(buffer_rx, buffer_rx_size);
-			for(uint8_t i = 0; i < bytes_read; ++i) {
-				if(buffer_rx[i] != expected_value) return HAL_ERROR;
-				expected_value += 1;
-				expected_value %= 256;
-				bytes_tested += 1;
-			}
-		} while(bytes_read != buffer_rx_size);
-	}
-
-	printf("FLASH: Tested %ld bytes\r\n", bytes_tested);
-	return HAL_OK;
-}
-
-HAL_StatusTypeDef Flash_Track_Status_Set(uint8_t track_number, uint8_t new_track_status) {
-	HAL_StatusTypeDef status;
-
-	if(track_number >= TRACK_AMOUNT) return HAL_ERROR;
-
-	status = BSP_QSPI_Write(&new_track_status, track_status_address[track_number], 1);
-	if(status != HAL_OK) return status;
-
-	return HAL_OK;
-}
-
-HAL_StatusTypeDef Flash_Track_Status_Get(uint8_t track_number, uint8_t* track_status) {
-	HAL_StatusTypeDef status;
-
-	if(track_number >= TRACK_AMOUNT) return HAL_ERROR;
-
-	status = BSP_QSPI_Read(track_status, track_status_address[track_number], 1);
-	if(status != HAL_OK) return status;
-
-	return HAL_OK;
-}
-
-HAL_StatusTypeDef Flash_Record_Start(uint8_t track_number) {
-	if(track_number >= TRACK_AMOUNT) return HAL_ERROR;
-
-	record_address = track_begin_address[track_number];
-	record_end_address = track_begin_address[track_number] + TRACK_SPACE_LENGTH;
-
-	return HAL_OK;
-}
-
-uint32_t Flash_Record_Save_Buffer(uint8_t* buffer, uint32_t buffer_length) {
-	if(buffer_length > record_end_address - record_address)
-		buffer_length = record_end_address - record_address;
-
-	if(BSP_QSPI_Write(buffer, record_address, buffer_length) != HAL_OK)
-		return 0;
-
-	record_address += buffer_length;
-
-	return buffer_length;
-}
-
-HAL_StatusTypeDef Flash_Play_Start(uint8_t track_number) {
-	if(track_number >= TRACK_AMOUNT) return HAL_ERROR;
-
-	play_address = track_begin_address[track_number];
-	play_end_address = track_begin_address[track_number] + TRACK_SPACE_LENGTH;
-
-	return HAL_OK;
-}
-
-uint32_t Flash_Play_Read_Buffer(uint8_t* buffer, uint32_t buffer_length) {
-	if(buffer_length > play_end_address - play_address)
-		buffer_length = play_end_address - play_address;
-
-	if(BSP_QSPI_Read(buffer, play_address, buffer_length) != HAL_OK)
-		return 0;
-
-	play_address += buffer_length;
-
-	return buffer_length;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
